a3853945618a837328ca9aeecdbff9261ef7dda4
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-07-03 11:43:15 +0200

    Add tests for the 'lines' format with the C generator

--- regtests/are-generator-c-tests.adb
+++ regtests/are-generator-c-tests.adb
@@ -21,6 +21,8 @@ with Ada.Directories;
 with Util.Test_Caller;
 package body Are.Generator.C.Tests is
 
+   Expect_Dir  : constant String := "regtests/expect/c/";
+
    function Tool return String;
 
    package Caller is new Util.Test_Caller (Test, "Are.Generator.C");
@@ -36,6 +38,8 @@ package body Are.Generator.C.Tests is
                        Test_Generate_C1'Access);
       Caller.Add_Test (Suite, "Test Are.Generate_C2",
                        Test_Generate_C2'Access);
+      Caller.Add_Test (Suite, "Test Are.Generate_Lines",
+                       Test_Generate_Lines'Access);
    end Add_Tests;
 
    procedure Test_Generate_C1 (T : in out Test) is
@@ -89,5 +93,83 @@ package body Are.Generator.C.Tests is
                                  & " {    color: #2a2a2a;  }", Result,
                                  "Invalid generation");
    end Test_Generate_C2;
+
+   procedure Test_Generate_Lines (T : in out Test) is
+      Dir        : constant String := Util.Tests.Get_Test_Path ("");
+      Rule       : constant String := "regtests/files/package-lines.xml";
+      Files      : constant String := "regtests/files";
+      Lines_H    : constant String := Ada.Directories.Compose (Dir, "lines.h");
+      Lines_C    : constant String := Ada.Directories.Compose (Dir, "lines.c");
+      Result     : Ada.Strings.Unbounded.Unbounded_String;
+   begin
+      --  Generate the lines.ads files
+      T.Execute (Tool & " -o " & Dir & " --lang=c --content-only --var-access --rule="
+                 & Rule & " " & Files & "/lines-empty", Result);
+
+      T.Assert (Ada.Directories.Exists (Lines_H),
+                "Resource file 'lines.h' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-empty.h"),
+         Test    => Lines_H,
+         Message => "Invalid lines-empty.h generation");
+
+      T.Assert (Ada.Directories.Exists (Lines_C),
+                "Resource file 'lines.c' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-empty.c"),
+         Test    => Lines_C,
+         Message => "Invalid lines-empty.c generation");
+
+      Ada.Directories.Delete_File (Lines_H);
+      Ada.Directories.Delete_File (Lines_C);
+      T.Execute (Tool & " -o " & Dir & " --lang=c --content-only --var-access --rule="
+                 & Rule & " " & Files & "/lines-single", Result);
+
+      T.Assert (Ada.Directories.Exists (Lines_H),
+                "Resource file 'lines.h' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-single.h"),
+         Test    => Lines_H,
+         Message => "Invalid lines-single.h generation");
+
+      T.Assert (Ada.Directories.Exists (Lines_C),
+                "Resource file 'lines.c' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-single.c"),
+         Test    => Lines_C,
+         Message => "Invalid lines-single.c generation");
+
+      Ada.Directories.Delete_File (Lines_H);
+      Ada.Directories.Delete_File (Lines_C);
+      T.Execute (Tool & " -o " & Dir & " --lang=c --content-only --var-access --rule="
+                 & Rule & " " & Files & "/lines-multiple", Result);
+
+      T.Assert (Ada.Directories.Exists (Lines_H),
+                "Resource file 'lines.h' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-multiple.h"),
+         Test    => Lines_H,
+         Message => "Invalid lines-multiple.h generation");
+
+      T.Assert (Ada.Directories.Exists (Lines_C),
+                "Resource file 'lines.c' not generated");
+
+      Util.Tests.Assert_Equal_Files
+        (T       => T,
+         Expect  => Util.Tests.Get_Path (Expect_Dir & "lines-multiple.c"),
+         Test    => Lines_C,
+         Message => "Invalid lines-multiple.c generation");
+
+   end Test_Generate_Lines;
 
 end Are.Generator.C.Tests;
