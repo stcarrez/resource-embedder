61ebf267adb21e4d0885e66c3cf9213c8c37fd82
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-07-03 22:47:33 +0200

    Update the c-lines examples to print the number of lines found in each script

--- examples/c-lines/src/show-script.c
+++ examples/c-lines/src/show-script.c
@@ -7,11 +7,11 @@
 
 int main() 
 {
-  printf("Create SQLite\n");
+  printf("Create SQLite %d lines\n", scripts_contents[Id_create_database].length);
   for (int i = 0; i < scripts_contents[Id_create_database].length; i++) {
     puts(scripts_contents[Id_create_database].content[i]);
   }
-  printf("Drop SQLite\n");
+  printf("Drop SQLite %d lines\n", scripts_contents[Id_drop_database].length);
   for (int i = 0; i < scripts_contents[Id_drop_database].length; i++) {
     puts(scripts_contents[Id_drop_database].content[i]);
   }
