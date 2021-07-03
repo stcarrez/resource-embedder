49033069454d244b6b8c682de6247e8352219237
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-07-03 11:43:46 +0200

    Add reference files for the C generator

---
+++ regtests/expect/c/lines-empty.c
@@ -0,0 +1,10 @@
+// Advanced Resource Embedder 1.1.0
+#include <string.h>
+#include "lines.h"
+
+static const char *const C_1[] = {};
+
+
+const struct lines_content lines_contents[] = {
+ { C_1, 0 }
+};
