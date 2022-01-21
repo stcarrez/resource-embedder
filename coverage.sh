ae62af74a320408394ffddf772d8feb567a0b546
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-01-21 22:02:01 +0100

    Update to exclude the Ada Util and Ada EL sources

--- coverage.sh
+++ coverage.sh
@@ -7,6 +7,8 @@ lcov --quiet --remove $NAME "/opt*" -o $NAME
 lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
 lcov --quiet --remove $NAME "*/regtests*" -o $NAME
 lcov --quiet --remove $NAME "*/b__*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-el/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-util/*" -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
