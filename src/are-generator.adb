a1faa71c74aa774a56c2e32aec442d16e1a9767e
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-02-25 16:58:23 +0100

    Change version to 1.5.0

--- src/are-generator.adb
+++ src/are-generator.adb
@@ -55,7 +55,7 @@ package body Are.Generator is
    --  ------------------------------
    function Get_Title return String is
    begin
-      return "Advanced Resource Embedder 1.4.0";
+      return "Advanced Resource Embedder 1.5.0";
    end Get_Title;
 
    --  ------------------------------
